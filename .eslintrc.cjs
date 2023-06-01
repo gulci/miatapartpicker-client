e0b8d923daf819f322e33d2a5ec743503aec9a7a
Mario Muñiz <mario@mariomuniz.com>
2023-05-31 21:02:47 -0700

    bootstrap project

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'next',
+  ],
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: path.join(__dirname, 'tsconfig.json'),
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: path.join(__dirname, 'tsconfig.json'),
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_'}],
+  },
+}
+
+module.exports = config
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSameLine: false,
+  bracketSpacing: false,
+  importOrder: ['^react$', '^next', '<THIRD_PARTY_MODULES>', '^(~)/', '^[.]?[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+}
