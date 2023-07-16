9af5b7a124555d80bffd44ef29d29f0e526f3a9f
Mario Muñiz <mario@mariomuniz.com>
2023-07-15 17:21:25 -0700

    add loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
+import {useEffect} from 'react'
+
 import type {NextPage} from 'next'
 import {type Session} from 'next-auth'
 import {SessionProvider, useSession} from 'next-auth/react'
 import {type AppType, type AppProps as BaseAppProps} from 'next/app'
+import {useRouter} from 'next/router'
 
 import {ChakraProvider} from '@chakra-ui/react'
 import {Analytics} from '@vercel/analytics/react'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 import {AuthSplashComponent} from '~/components/auth/AuthSplashComponent'
 import {MainLayout} from '~/layouts/MainLayout'
@@ -24,8 +29,39 @@ const MiataPartPickerClient: AppType<{session: Session | null}> = ({
   Component,
   pageProps: {session, ...pageProps},
 }: AppProps) => {
+  const {events: routerEvents} = useRouter()
+
+  // Configure loading bar
+  NProgress.configure({
+    showSpinner: false,
+  })
+
   // Use the layout defined at the page-level, if available.
   const getLayout = Component.getLayout ?? ((page) => <MainLayout>{page}</MainLayout>)
+
+  useEffect(() => {
+    function startLoader() {
+      NProgress.start()
+    }
+
+    routerEvents.on('routeChangeStart', startLoader)
+
+    return () => {
+      routerEvents.off('routeChangeStart', startLoader)
+    }
+  }, [routerEvents])
+
+  useEffect(() => {
+    function stopLoader() {
+      NProgress.done()
+    }
+
+    routerEvents.on('routeChangeComplete', stopLoader)
+
+    return () => {
+      routerEvents.off('routeChangeComplete', stopLoader)
+    }
+  }, [routerEvents])
 
   return (
     <>
