6fba8784faa90e1590ea008a0b1fc2938a77129f
Mario Muñiz <mario@mariomuniz.com>
2023-07-16 13:07:35 -0700

    improve builds page perf

--- src/pages/users/[userId]/builds.tsx
+++ src/pages/users/[userId]/builds.tsx
@@ -1,31 +1,49 @@
-import {type ReactElement, useState} from 'react'
+import {type ReactElement, useEffect, useState} from 'react'
 
 import {type GetServerSideProps, type InferGetServerSidePropsType} from 'next'
 import Image from 'next/image'
 import NextLink from 'next/link'
 import {useRouter} from 'next/router'
 
-import {AspectRatio, Center, Divider, Flex, Heading, Link, List, ListItem, Stack, Text, VStack} from '@chakra-ui/react'
+import {
+  AspectRatio,
+  Center,
+  Divider,
+  Flex,
+  Heading,
+  Link,
+  List,
+  ListItem,
+  Skeleton,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
 
 import {env} from '~/env.mjs'
 import {UserLayout} from '~/layouts/UserLayout'
 import {type AppPage} from '~/pages/_app'
-import {getUserBuilds} from '~/server/mpp/builds'
-import {type Build} from '~/server/mpp/types/builds'
 import {type Profile} from '~/server/mpp/types/users'
 import {getUserProfile} from '~/server/mpp/users'
 import {trpc} from '~/utils/trpc'
 
-const BuildsPage: AppPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({builds: ssrBuilds}) => {
+const BuildsPage: AppPage<InferGetServerSidePropsType<typeof getServerSideProps>> = () => {
   const {
     query: {userId},
   } = useRouter()
-  const {data: builds} = trpc.mpp.getUserBuilds.useQuery({userId: userId as string}, {initialData: ssrBuilds})
+  const {data: builds, fetchStatus: buildsFetchStatus} = trpc.mpp.getUserBuilds.useQuery({userId: userId as string})
   const [selectedBuildUid, setSelectedBuildUid] = useState<string | null>(
-    builds.length > 0 && builds[0] ? builds[0].uid : null,
+    !!builds && builds.length > 0 && builds[0] ? builds[0].uid : null,
   )
-  const selectedBuild = builds.find((b) => b.uid === selectedBuildUid)
+  const selectedBuild = builds?.find((b) => b.uid === selectedBuildUid)
   const selectedBuildBannerPhoto = selectedBuild?.photos.find((p) => selectedBuild.banner_photo_id === p.uuid)
+
+  useEffect(() => {
+    // select first build on first load
+    if (!selectedBuildUid && !!builds && builds.length > 0 && builds[0]) {
+      setSelectedBuildUid(builds[0].uid)
+    }
+  }, [builds, selectedBuildUid])
 
   return (
     <Stack direction={{base: 'column', lg: 'row'}} flex="1" spacing={{base: '12', lg: '16'}}>
@@ -36,54 +54,64 @@ const BuildsPage: AppPage<InferGetServerSidePropsType<typeof getServerSideProps>
           <VStack alignItems="start" spacing="4" width="full">
             <VStack alignItems="start" spacing="1" width="full">
               <Text as="b">Builds</Text>
-              <List width="full">
-                {builds.map((build) => (
-                  <ListItem
-                    _dark={{backgroundColor: build.uid === selectedBuildUid ? 'gray.600' : 'gray.700'}}
-                    _light={{backgroundColor: build.uid === selectedBuildUid ? 'gray.200' : 'gray.100'}}
-                    _hover={{
-                      _dark: {backgroundColor: 'gray.600'},
-                      _light: {backgroundColor: 'gray.200'},
-                    }}
-                    cursor="pointer"
-                    key={build.uid}
-                    onClick={() => {
-                      const b = builds.find((b) => b.uid === build.uid)
-                      b && setSelectedBuildUid(b.uid)
-                    }}
-                    padding="2"
-                    role="button"
-                  >
-                    <Text as="b" fontSize="sm">
-                      {build.description || 'Untitled Build'}
-                    </Text>
-                  </ListItem>
-                ))}
-              </List>
+              {!builds ? (
+                buildsFetchStatus === 'fetching' && (
+                  <VStack width="full">
+                    <Skeleton height="20px" width="full" />
+                    <Skeleton height="20px" width="full" />
+                    <Skeleton height="20px" width="full" />
+                  </VStack>
+                )
+              ) : (
+                <List width="full">
+                  {builds.map((build) => (
+                    <ListItem
+                      _dark={{backgroundColor: build.uid === selectedBuildUid ? 'gray.600' : 'gray.700'}}
+                      _light={{backgroundColor: build.uid === selectedBuildUid ? 'gray.200' : 'gray.100'}}
+                      _hover={{
+                        _dark: {backgroundColor: 'gray.600'},
+                        _light: {backgroundColor: 'gray.200'},
+                      }}
+                      cursor="pointer"
+                      key={build.uid}
+                      onClick={() => {
+                        const b = builds.find((b) => b.uid === build.uid)
+                        b && setSelectedBuildUid(b.uid)
+                      }}
+                      padding="2"
+                      role="button"
+                    >
+                      <Text as="b" fontSize="sm">
+                        {build.description || 'Untitled Build'}
+                      </Text>
+                    </ListItem>
+                  ))}
+                </List>
+              )}
             </VStack>
           </VStack>
         </VStack>
       </Flex>
       <Flex flex="1">
-        {selectedBuild && (
-          <VStack alignItems="start" spacing="4" flex="1">
-            <Center width="full">
-              <NextLink
-                href={{
-                  pathname: '/car/[buildId]',
-                  query: {buildId: selectedBuild.uid},
-                }}
-                legacyBehavior
-                passHref
-              >
-                <Link>
-                  <Heading size="lg" textAlign="center">
-                    {selectedBuild.description || 'Untitled Build'}
-                  </Heading>
-                </Link>
-              </NextLink>
-            </Center>
-            {selectedBuildBannerPhoto && (
+        <VStack alignItems="start" spacing="4" flex="1">
+          {selectedBuild && selectedBuildBannerPhoto && !!builds ? (
+            <>
+              <Center width="full">
+                <NextLink
+                  href={{
+                    pathname: '/car/[buildId]',
+                    query: {buildId: selectedBuild.uid},
+                  }}
+                  legacyBehavior
+                  passHref
+                >
+                  <Link>
+                    <Heading size="lg" textAlign="center">
+                      {selectedBuild.description || 'Untitled Build'}
+                    </Heading>
+                  </Link>
+                </NextLink>
+              </Center>
               <AspectRatio ratio={16 / 9} width="full">
                 <Image
                   alt={selectedBuildBannerPhoto.filename}
@@ -94,9 +122,18 @@ const BuildsPage: AppPage<InferGetServerSidePropsType<typeof getServerSideProps>
                   width={selectedBuildBannerPhoto.meta.width}
                 />
               </AspectRatio>
-            )}
-          </VStack>
-        )}
+            </>
+          ) : (
+            buildsFetchStatus === 'fetching' && (
+              <>
+                <Center width="full">
+                  <Skeleton height="8" width="50%" />
+                </Center>
+                <Skeleton height="400px" width="100%" />
+              </>
+            )
+          )}
+        </VStack>
       </Flex>
     </Stack>
   )
@@ -114,18 +151,14 @@ export default BuildsPage
 
 type BuildsPageProps = {
   profile: Profile
-  builds: Build[]
 }
 
 export const getServerSideProps: GetServerSideProps<BuildsPageProps, {userId: string}> = async (ctx) => {
   if (!ctx.params) return {notFound: true}
   const profile = await getUserProfile(ctx.params.userId)
   if (!profile) return {notFound: true}
-  const builds = await getUserBuilds(ctx.params.userId)
-  if (!builds) return {notFound: true}
   return {
     props: {
-      builds,
       profile,
     },
   }
